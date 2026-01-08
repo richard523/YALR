d056140bdd25cea87e1a572eb19190fba949ea58
richard523 <ricgat523@gmail.com>
2026-01-08 15:50:27 -0600

    155 minstack lc

---
+++ 155.min-stack.ts
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=155 lang=typescript
+ *
+ * [155] Min Stack
+ */
+
+// @lc code=start
+export class MinStack {
+    private stack: { val: number; min: number }[];
+
+    constructor() {
+        this.stack = [];
+    }
+
+    push(val: number): void {
+        const currentMin = this.stack.length === 0
+            ? val
+            : Math.min(val, this.getMin());
+
+        this.stack.push({ val, min: currentMin });
+    }
+
+    pop(): void {
+        this.stack.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1].val;
+    }
+
+    getMin(): number {
+        return this.stack[this.stack.length - 1].min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+// @lc code=end
